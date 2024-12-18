71641c62d31379d466d68743bcc8ea7f49c89c4d
yuramishin <robothot@yandex.ru>
2024-12-18 16:03:49 +0000

    add autoincrenet

--- app/models/base.py
+++ app/models/base.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class BaseModel:
-    id: Mapped[datetime] = mapped_column(Integer, primary_key=True)
+    id: Mapped[datetime] = mapped_column(Integer, primary_key=True, autoincrement=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now())
     update_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)
