96f8202188574a50600a9675f9a6a6f64391b42c
yuramishin <robothot@yandex.ru>
2024-12-18 17:20:39 +0000

    Пофиксил бак, убрал базовую модель

--- app/models/users.py
+++ app/models/users.py
@@ -4,9 +4,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID as U_UUID
 
 from app.db import Base
+from app.models.base import BaseModel
 
 
-class User(Base):
+class User:
     __tablename__ = 'users'
 
     uuid: Mapped[UUID] = mapped_column(U_UUID(as_uuid=True), default=uuid4, unique=True)
