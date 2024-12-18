8a948219ef9f8095cf7c72c2953cdcefb33f77d0
yuramishin <robothot@yandex.ru>
2024-12-18 15:52:54 +0000

    Внес правки в наимерование модели, изменил структуру базовой модели и модели пользователя

--- app/models/base_model.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from sqlalchemy import Column, DateTime, Integer
-
-
-class BaseModel:
-    id = Column(Integer, primary_key=True)
-    update_at = Column(
-        DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False
-    )
--- app/models/users.py
+++ app/models/users.py
@@ -1,8 +1,7 @@
 from uuid import UUID, uuid4
-from sqlalchemy import DateTime, String
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID as U_UUID
-from datetime import datetime
 
 from app.db import Base
 
@@ -10,7 +9,6 @@ from app.db import Base
 class User(Base):
     __tablename__ = 'users'
 
-    uuid:               Mapped[UUID] = mapped_column(U_UUID(as_uuid=True), primary_key=True, default=uuid4)
-    username:           Mapped[str] = mapped_column(String(256), nullable=False, unique=True)
-    created_at:         Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now())
-    hashed_password:    Mapped[bool] = mapped_column(String(256), nullable=False)
+    uuid: Mapped[UUID] = mapped_column(U_UUID(as_uuid=True), primary_key=True, default=uuid4)
+    username: Mapped[str] = mapped_column(String(256), nullable=False, unique=True)
+    password: Mapped[bool] = mapped_column(String(256), nullable=False)
