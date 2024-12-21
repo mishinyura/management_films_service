1ad102bc0a2c0cf03a083e50c11731ee3d6f7f93
shynadja <149534941+shynadja@users.noreply.github.com>
2024-12-21 23:26:33 +0300

    Update main.py

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import uvicorn
-from .logger import setup_logger
-
-app = FastAPI()
-setup_logger()  # Вызываем функцию настройки логгера
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
