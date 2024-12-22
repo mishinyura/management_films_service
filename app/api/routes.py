bb76cf61b54e564da28ae8804e423df06b6efdbc
Kate Samuseva <ksamuseva@Mac-Kate.local>
2024-12-22 12:21:57 +0300

    api films

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
 from app.api.login import router as login
+from app.api.films_api import films_router
 
 
 api_router = APIRouter()
 api_router.include_router(login)
+
+ROUTES = {
+    "/films": films_router,
+}
